1e58ff6b494cab43ce1df1f2fe9993c70c11387f
Rafael Oliveira <r0liveira@icloud.com>
2026-05-12 07:54:13 -0400

    git: author fix

--- home-manager/modules/programs/git.nix
+++ home-manager/modules/programs/git.nix
@@ -1,6 +1,10 @@
 {
   programs = {
     eza.enable = true;
-    git.enable = true;
+    git = {
+      enable = true;
+      userName = "skydive420dz";
+      userEmail = "r0liveira@icloud.com";
+    };
   };
 }
