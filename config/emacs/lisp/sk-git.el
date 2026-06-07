084323ef3087cc7d63912cfb7dbfc5781c07a61f
Rafael Oliveira <r0liveira@icloud.com>
2026-06-07 03:21:07 -0400

    Restore Magit section tab bindings

--- config/emacs/lisp/sk-git.el
+++ config/emacs/lisp/sk-git.el
@@ -14,7 +14,12 @@
   :bind (("C-c g" . magit-status))
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
-  (evil-collection-init 'magit))
+  (evil-collection-init 'magit)
+  (evil-define-key '(normal motion) magit-mode-map
+    (kbd "TAB") #'magit-section-toggle
+    (kbd "<tab>") #'magit-section-toggle
+    (kbd "S-TAB") #'magit-section-cycle-global
+    (kbd "<backtab>") #'magit-section-cycle-global))
 
 (provide 'sk-git)
 
