d03fecbbc8444ed851d1fbbcab6ab901ea782be2
Rafael Oliveira <r0liveira@icloud.com>
2026-09-01 22:22:00 -0400

    Fix flake update alias

--- home-manager/modules/programs/zsh.nix
+++ home-manager/modules/programs/zsh.nix
@@ -1,15 +1,15 @@
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
 
-    shellAliases = {
-      ls = "ls -G -F";
-      vim = "nvim";
-      nrs = "sudo darwin-rebuild switch --flake ~/Projects/nixos-macos";
-      nuf = "nix flake update --flake ~/Projects/nixos-macos";
+        shellAliases = {
+            ls = "ls -G -F";
+            vim = "nvim";
+            nrs = "sudo darwin-rebuild switch --flake ~/Projects/nixos-macos";
+            nfu = "nix flake update --flake ~/Projects/nixos-macos";
+        };
     };
-  };
 }
